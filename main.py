886ccc83f81fab8da3971f21c44b14d75f129f72
Abhishek Gupta <abhishek.gupta2@tothenew.com>
2025-08-15 06:09:22 +0530

    Initial backend commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "statics")
+
+app.mount("/statics", StaticFiles(directory=STATIC_DIR), name="statics")
