cc87ed39a404fa92130c76fad98770dd559f887e
Abhishek Gupta <abhishek.gupta2@tothenew.com>
2025-08-15 06:45:49 +0530

    requirment

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_DIR = os.path.join(BASE_DIR, "statics")
 
 app.mount("/statics", StaticFiles(directory=STATIC_DIR), name="statics")
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI backend is running!"}
